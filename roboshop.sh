369bcb3a352a6fcbc7e2628b1196f814452515d9
Teja Sri <iamtejasri01@gmail.com>
2026-02-12 23:54:43 +0530

    adding files

---
+++ roboshop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SG_ID="sg-01e4ec5fcb9ead862"
+AMI_ID="ami-0220d79f3f480ecf5"
+for instance in $@
+do 
+   aws ec2 run-instance --image-id $AMI_ID --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$instance}] --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
+done
