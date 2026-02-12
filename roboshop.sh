415ae8d1cb3a6541401c4ce31e22cd090254ca13
Teja Sri <iamtejasri01@gmail.com>
2026-02-13 00:15:03 +0530

    adding text

--- roboshop.sh
+++ roboshop.sh
@@ -4,5 +4,5 @@ SG_ID="sg-01e4ec5fcb9ead862"
 AMI_ID="ami-0220d79f3f480ecf5"
 for instance in $@
 do 
-   aws ec2 run-instance --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$instance}] --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
+   aws ec2 run-instance --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$instance}]" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
 done
